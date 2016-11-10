b4551422a0095a41e64914a86d07a1e944555518
ShuyangLiu <sliu44@u.rochester.edu>
2016-11-10 11:44:27 -0500

    add simple binary tree structure

---
+++ binary_tree.rb
@@ -0,0 +1,34 @@
+# A simple implementation of binary tree for question 03 part b
+class Node
+  attr_accessor :data, :left_child, :right_child
+  def initialize (d)
+    @data = d
+  end
+  def add(node)
+    if self.left_child == nil
+      self.left_child = node
+    elsif self.right_child == nil
+      self.right_child = node
+    else
+      self.left_child.add(node)
+    end
+  end
+end
+
+class BinaryTree
+  attr_accessor :root
+  def initialize
+    @root = nil
+  end
+  def add_node(data)
+    node = Node.new(data)
+    if @root == nil
+      @root = node
+    else
+      @root.add(node)
+    end
+  end
+end
+
+
+
